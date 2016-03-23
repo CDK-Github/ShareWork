b607d36065675cb6d47bddbc8dda5e2451e064ee
Cédric LEVEQUE <cedric-leveque@laposte.net>
2016-03-23 09:50:02 +0000

    Entité utilisateur

---
+++ sharework/src/CoreBundle/Resources/config/doctrine/User.orm.php
@@ -0,0 +1,27 @@
+<?php
+
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+
+$metadata->setInheritanceType(ClassMetadataInfo::INHERITANCE_TYPE_NONE);
+$metadata->customRepositoryClassName = 'CoreBundle\Repository\UserRepository';
+$metadata->setChangeTrackingPolicy(ClassMetadataInfo::CHANGETRACKING_DEFERRED_IMPLICIT);
+$metadata->mapField(array(
+   'fieldName' => 'id',
+   'type' => 'integer',
+   'id' => true,
+   'columnName' => 'id',
+  ));
+$metadata->mapField(array(
+   'columnName' => 'email',
+   'fieldName' => 'email',
+   'type' => 'string',
+   'length' => '50',
+   'unique' => true,
+  ));
+$metadata->mapField(array(
+   'columnName' => 'password',
+   'fieldName' => 'password',
+   'type' => 'string',
+   'length' => '16',
+  ));
+$metadata->setIdGeneratorType(ClassMetadataInfo::GENERATOR_TYPE_AUTO);
